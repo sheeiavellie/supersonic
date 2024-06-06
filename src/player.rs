77a6174ffddf68316d709cc7a440342bcbd0e63c
Alexander V. Trotsky <josephlandmars@gmail.com>
2024-06-06 18:12:21 +0300

    fix: small fix

--- src/player.rs
+++ src/player.rs
@@ -10,7 +10,7 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(Startup, spawn_player)
-            .add_systems(Update, (player_movement));
+            .add_systems(Update, player_movement);
     }
 }
 
