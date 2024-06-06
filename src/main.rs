0d43cebab1017234e23841763488163c34c630f4
Alexander V. Trotsky <josephlandmars@gmail.com>
2024-06-06 17:48:40 +0300

    fix: fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ mod camera;
 mod world;
 mod post_processing;
 mod materials;
+mod ui;
 
 use player::PlayerPlugin;
 use camera::CameraPlugin;
 use world::WorldPlugin;
 use post_processing::PostProcessPlugin;
 use materials::DefinedMaterialsPlugin;
+use ui::UIPlugin;
 
 fn main() {
     let mut app = App::new();
@@ -38,6 +40,7 @@ fn main() {
         ThirdPersonCameraPlugin,
         PostProcessPlugin,
         DefinedMaterialsPlugin,
+        UIPlugin,
     ));
 
     app.run();
