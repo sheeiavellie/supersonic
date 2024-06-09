f86dafc19eb3f64c528dc6687f2b951953a0e095
Alexander V. Trotsky <josephlandmars@gmail.com>
2024-06-09 20:26:44 +0300

    feat: add did_switch_camera_mode test

--- src/tests/post_processing.rs
+++ src/tests/post_processing.rs
@@ -1,5 +1,47 @@
+use bevy::{app::App, prelude::*};
+
+use crate::{
+    camera::{update_post_processing, IsPostProcessingActive}, 
+    post_processing::PostProcessSettings
+};
+
 #[test]
-fn it_works() {
-    let result = 2;
-    assert_eq!(result, 2);
+fn did_switch_camera_mode() {
+    let mut app = App::new();
+
+    app.add_systems(Update, update_post_processing);
+
+    let main_camera = app.world
+        .spawn((
+            IsPostProcessingActive(false),
+            PostProcessSettings {
+                intensity: 0.0,
+            },
+        ))
+        .id();
+
+    assert!(app.world.get::<IsPostProcessingActive>(main_camera).is_some());
+    
+    let mut input = ButtonInput::<KeyCode>::default();
+    input.press(KeyCode::KeyJ);
+    app.insert_resource(input.clone());
+
+    app.world.resource_mut::<ButtonInput<KeyCode>>().release(KeyCode::KeyJ);
+
+    app.update();
+
+    assert_eq!(app.world.get::<IsPostProcessingActive>(main_camera).unwrap().0, true);
+
+    app.world.resource_mut::<ButtonInput<KeyCode>>().clear();
+
+    app.update();
+
+    input.press(KeyCode::KeyJ);
+    app.insert_resource(input.clone());
+
+    app.world.resource_mut::<ButtonInput<KeyCode>>().release(KeyCode::KeyJ);
+
+    app.update();
+
+    assert_eq!(app.world.get::<IsPostProcessingActive>(main_camera).unwrap().0, false);
 }
